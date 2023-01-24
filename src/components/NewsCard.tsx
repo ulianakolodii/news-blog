918f7a7a8807e3ad05e0423a0f9dae2eb1d3e6e1
Uliana Kolodii <ulianakolodii@gmail.com>
2023-01-25 01:11:12 +0200

    feat: add filter and highliter

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,4 +1,5 @@
-import { NewsItem } from "../api/news";
+import { FC } from "react";
+import { Article } from "../api";
 import { Link } from "react-router-dom";
 import {
   Box,
@@ -9,87 +10,101 @@ import {
   CardActions,
   Button,
 } from "@mui/material";
-import { CalendarToday } from "@mui/icons-material";
-import { ArrowForward } from "@mui/icons-material";
-// import dayjs from "dayjs";
-// import relativeTime from "dayjs/plugin/relativeTime";
+import { ReactComponent as CalendarToday } from "../assets/icons/calendar.svg";
+import { ReactComponent as ArrowForward } from "../assets/icons/arrow-forward.svg";
+import dayjs from "dayjs";
 
-// dayjs().format();
-// dayjs.extend(relativeTime);
+const truncate = (str: string, length: number) => {
+  if (str.length > length) {
+    return str.substring(0, length) + "...";
+  }
+  return str;
+};
 
-const NewsCard = ({ title, imageUrl, summary, publishedAt }: NewsItem) => {
+const NewsCard: FC<Article & { keywordsAsMap: Record<string, true> }> = ({
+  id,
+  title,
+  imageUrl,
+  summary,
+  keywordsAsMap,
+  publishedAt,
+}) => {
+  const highlightedTitle = title.split(" ").map((word) => {
+    if (keywordsAsMap[word.toLowerCase()]) {
+      return <span style={{ background: "yellow" }}>{word}</span>;
+    }
+    return ` ${word} `;
+  });
+  const truncatedSummary = truncate(summary || "", 100);
+  const highlightedSummary = truncatedSummary.split(" ").map((word) => {
+    if (keywordsAsMap[word.toLowerCase()]) {
+      return <span style={{ background: "yellow" }}>{word}</span>;
+    }
+    return ` ${word} `;
+  });
   return (
-    <Box sx={{ marginTop: 4.5 }}>
-      <Card
-        sx={{
-          maxWidth: 400,
-          border: "1px solid",
-          borderColor: "primary.main",
-          boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.05)",
-          borderRadius: "5px",
-        }}
-      >
-        <CardMedia sx={{ height: 217 }} image={imageUrl} />
-        <CardContent sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 1,
-            }}
-          >
-            <CalendarToday
-              sx={{ height: "13px", width: "12px", color: "text.secondary" }}
-            />
-            <Typography color="text.secondary" fontSize={14}>
-              {/* {dayjs().to(dayjs(publishedAt))} */}
-            </Typography>
-          </Box>
-          <Typography variant="h5" component="div" color="secondary.main">
-            {title}
+    <Card
+      sx={{
+        maxWidth: 400,
+        border: "1px solid",
+        borderColor: "primary.main",
+        boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.05)",
+        borderRadius: 1.25,
+        flex: 1,
+        flexBasis: "100%",
+      }}
+    >
+      <CardMedia sx={{ height: 217 }} image={imageUrl} />
+      <CardContent sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 1,
+          }}
+        >
+          <CalendarToday />
+          <Typography color="text.secondary" fontSize={14}>
+            {dayjs(publishedAt).format("MMM D, YYYY")}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {summary}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button
-            variant="text"
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: "4px",
-            }}
-          >
-            <Link to="/readmore" style={{ textDecoration: "none" }}>
-              <Typography
-                sx={{
-                  textTransform: "lowercase",
-                  color: "secondary.main",
-                  fontWeight: 700,
-                  fontSize: 16,
-                  ":first-letter": {
-                    textTransform: "uppercase",
-                  },
-                }}
-              >
-                Read more
-              </Typography>
-            </Link>
-            <ArrowForward
+        </Box>
+        <Typography variant="h5" component="div" color="secondary.main">
+          {highlightedTitle}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {highlightedSummary}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant="text"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 0.75,
+          }}
+        >
+          <Link to={`/readmore/${id}`} style={{ textDecoration: "none" }}>
+            <Typography
               sx={{
-                height: "10px",
-                width: "12px",
+                textTransform: "lowercase",
                 color: "secondary.main",
-                marginTop: "2px",
+                fontWeight: 700,
+                fontSize: 16,
+                ":first-letter": {
+                  textTransform: "uppercase",
+                },
               }}
-            />
-          </Button>
-        </CardActions>
-      </Card>
-    </Box>
+            >
+              Read more
+            </Typography>
+          </Link>
+          <ArrowForward />
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
