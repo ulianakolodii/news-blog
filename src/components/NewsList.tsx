25b5214159e443e40cc1c1e74b4a712f359bd346
Uliana Kolodii <ulianakolodii@gmail.com>
2025-01-02 21:28:16 +0200

    chore: delete logs

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -7,41 +7,38 @@ const NewsList: FC<{
   data?: Articles;
   keywordsAsMap: Record<string, true>;
 }> = ({ data, keywordsAsMap }) => (
-  console.log("log data", data),
-  (
-    <>
-      <Box
-        sx={{
-          marginTop: 4,
-          display: "flex",
-          flexDirection: "column",
-          gap: 0.5,
-        }}
-      >
-        <Typography sx={{ fontWeight: 600 }}>
-          Results: {data ? data?.results?.length : 0}
-        </Typography>
-        <Divider />
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "flex-start",
-          gap: 4.5,
-          marginTop: 4.5,
-        }}
-      >
-        {data &&
-          data.results?.map((newsItem) => (
-            <NewsCard
-              key={newsItem.id}
-              {...newsItem}
-              keywordsAsMap={keywordsAsMap}
-            />
-          ))}
-      </Box>
-    </>
-  )
+  <>
+    <Box
+      sx={{
+        marginTop: 4,
+        display: "flex",
+        flexDirection: "column",
+        gap: 0.5,
+      }}
+    >
+      <Typography sx={{ fontWeight: 600 }}>
+        Results: {data ? data?.results?.length : 0}
+      </Typography>
+      <Divider />
+    </Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        gap: 4.5,
+        marginTop: 4.5,
+      }}
+    >
+      {data &&
+        data.results?.map((newsItem) => (
+          <NewsCard
+            key={newsItem.id}
+            {...newsItem}
+            keywordsAsMap={keywordsAsMap}
+          />
+        ))}
+    </Box>
+  </>
 );
 export default NewsList;
